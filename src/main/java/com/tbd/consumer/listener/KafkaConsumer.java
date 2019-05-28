ad924f4888405a6e9a01f8b522d9080c754f75a9
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2019-05-28 15:44:39 -0400

    MongoDB actualizado
    Refactor en guardado de objeto en mongodb

--- src/main/java/com/tbd/consumer/listener/KafkaConsumer.java
+++ src/main/java/com/tbd/consumer/listener/KafkaConsumer.java
@@ -1,30 +1,33 @@
 package com.tbd.consumer.listener;
 
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
 import com.tbd.consumer.model.Tweet;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 
 @Service
 public class KafkaConsumer {
-    @Value("${zookeeper.group-id}")
-    private static String zookeeperGroupId;
+
     MongoClient mongoClient = MongoClients.create();
-    MongoDatabase data = mongoClient.getDatabase("twitter");
-    //hola
-    MongoCollection<Document> tweetCollection = data.getCollection("twitt");
-    @KafkaListener(topics = "twitterJsonChile", groupId = "0", containerFactory = "tweetKafkaListenerFactory")
-    public void consumer(Tweet tweet){
-        Document doc = new Document();
+    MongoDatabase data = mongoClient.getDatabase("ligaChilenaDB");
+    MongoCollection<Document> tweetCollection = data.getCollection("ligaChilena");
+
+    @KafkaListener(topics = "${kafka.topic}", groupId = "${zookeeper.group-id}", containerFactory = "tweetKafkaListenerFactory")
+    public void consumer(Tweet tweet)
+    {
+        Gson gson = new Gson();
+        String json = gson.toJson(tweet);
+        BasicDBObject document = (BasicDBObject) JSON.parse(json);
+        tweetCollection.insertOne(new Document(document));
+
+        /*Document doc = new Document.parse(json);
         doc.put("id", tweet.getId());
         doc.put("name", tweet.getName());
         doc.put("text", tweet.getText());
@@ -35,7 +38,7 @@ public class KafkaConsumer {
         doc.put("retweet", tweet.getRetweet());
         doc.put("publicationDate", tweet.getPublicationDate());
         tweetCollection.insertOne(doc);
-        System.out.println(doc.toString());
+        System.out.println(doc.toString());*/
         //tweetCollection.insertOne();
         //System.out.println("Consume message"+ tweet.toString());
     }
